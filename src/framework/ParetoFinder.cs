4ccd6947de20e7abd38b021b46963bd9d270d0f0
NichtNachfragen <NichtNachfragen@e166cc3d-863f-0410-b885-71d28630af16>
2007-12-03 20:07:46 +0000

    Enhanced 3D-Paretofinder, decreased runtime drastically, tieded up the code and replaced the ParetoFinder-ProgressBar with the ProgressBar in the MainWindow

--- src/framework/ParetoFinder.cs
+++ src/framework/ParetoFinder.cs
@@ -48,175 +48,160 @@ namespace Pavel.Framework {
         /// Pareto-Front with {3,4} (in the 2D-Case).</param>
         public static Selection EvaluateParetoFront(PointSet pointSet, params ColumnProperty[] cp) {
             if (cp.Length == 2) {
-                return Sort(pointSet, true, cp[0], cp[1]);
+                return Sort(pointSet, cp[0], cp[1]);
             } else if (cp.Length == 3) {
-                return Sort(pointSet, true, cp[0], cp[1], cp[2]);
+                return Sort(pointSet, cp[0], cp[1], cp[2]);
             } else {
                 PavelMain.LogBook.Error("Invalid Column-Indices for evaluating the Pareto Front", true);
                 return null;
             }
         }
         
-        //------------------------2D: works and is fast-------------------------------------------------
-        private static Selection Sort(PointSet pointSet, bool onlyPareto, ColumnProperty cp0, ColumnProperty cp1) {
+        /// <summary>
+        /// Performs the paretofinding for two dimensions
+        /// Runtime is O(n*log(n))
+        /// </summary>
+        /// <param name="pointSet">pointset to calculated the paretofront</param>
+        /// <param name="cp0">first dimension</param>
+        /// <param name="cp1">second dimension</param>
+        /// <returns>a selection containing the pareto-optimal points</returns>
+        private static Selection Sort(PointSet pointSet, ColumnProperty cp0, ColumnProperty cp1) {
 
-            SortablePoint[] sorted = new SortablePoint[pointSet.Length];
+            ParetoPoint[] sorted = new ParetoPoint[pointSet.Length];
 
-            Pavel.GUI.ParetoFinderWindow paretoFinderWindow = new Pavel.GUI.ParetoFinderWindow();
-            paretoFinderWindow.Show();
+            //TODO: Mixing gui-code within the framework is not really nice, replace this by events
+            PavelMain.MainWindow.StatusBar.StartProgressBar(0, sorted.Length, "Calculating Paretofront...");
 
-            int index = 0;
+            //Create an array containing the scaled points
             foreach (PointList pl in pointSet.PointLists) {
                 int column0 = pl.ColumnSet.IndexOf(cp0.Column);
                 int column1 = pl.ColumnSet.IndexOf(cp1.Column);
                 for (int i = 0; i < pl.Count; i++) {
-                    sorted[index] = new SortablePoint(new double[] { pl[i].ScaledValue(column0,cp0), pl[i].ScaledValue(column1,cp1) }, pl[i], 1);
-                    index++;
+                    sorted[i] = new ParetoPoint(new double[] { pl[i].ScaledValue(column0,cp0), pl[i].ScaledValue(column1,cp1) }, pl[i]);
                 }
             }
+
             Array.Sort(sorted, Comparer2D);
-            double min = sorted[0].Values[1];
+            double min = sorted[0].values[1];
             Selection paretoFront = new Selection();
             paretoFront.Active = true;
             paretoFront.Label = "Pareto-Front";
 
-            paretoFinderWindow.setParetoFinderWindow(sorted.Length - 1, 1);
-
             for (int i = 0; i < sorted.Length; i++) {
-                if (min >= sorted[i].Values[1]) {
-                    paretoFront.Add(sorted[i].Point);
-                    min = sorted[i].Values[1];
-                    paretoFinderWindow.progressBarStep();
-                    paretoFinderWindow.Refresh();
+                if ( i % 100 == 0 ) PavelMain.MainWindow.StatusBar.IncrementProgressBar(100);
+                if (min >= sorted[i].values[1]) {
+                    paretoFront.Add(sorted[i].point);
+                    min = sorted[i].values[1];
                 }
             }
-            paretoFinderWindow.Close();
+            PavelMain.MainWindow.StatusBar.EndProgressBar();
             return paretoFront;  
         }
-
-        //------------------ 3D: works, but is slow for many points----------------------------------------------------------
-        private static Selection Sort(PointSet pointSet, bool onlyPareto, ColumnProperty cp0, ColumnProperty cp1, ColumnProperty cp2) {
-            SortablePoint[] sorted = new SortablePoint[pointSet.Length];
-            int index = 0;
+        
+        /// <summary>
+        /// Performs the paretofinding for three dimensions
+        /// Worst case runtime is O(n^2), when all points are already pareto-optimal
+        /// Typically it is much faster and scales more or less with the size of the pareto-optimal set
+        /// </summary>
+        /// <param name="pointSet">pointset to calculated the paretofront</param>
+        /// <param name="cp0">first dimension</param>
+        /// <param name="cp1">second dimension</param>
+        /// <param name="cp2">third dimension</param>
+        /// <returns>a selection containing the pareto-optimal points</returns>
+        private static Selection Sort(PointSet pointSet, ColumnProperty cp0, ColumnProperty cp1, ColumnProperty cp2) {
+            ParetoPoint[] points = new ParetoPoint[pointSet.Length];
+            //Create an array containing the scaled points
             foreach (PointList pl in pointSet.PointLists) {
                 int column0 = pl.ColumnSet.IndexOf(cp0.Column);
                 int column1 = pl.ColumnSet.IndexOf(cp1.Column);
                 int column2 = pl.ColumnSet.IndexOf(cp2.Column);
                 for (int i = 0; i < pl.Count; i++) {
-                    //TODO ScaledValue is slow
-                    sorted[index] = new SortablePoint(new double[] { pl[i].ScaledValue(column0,cp0), pl[i].ScaledValue(column1,cp1), pl[i].ScaledValue(column2,cp2) }, pl[i], 1);
-                    index++;
+                    points[i] = new ParetoPoint(new double[] { pl[i].ScaledValue(column0,cp0), pl[i].ScaledValue(column1,cp1), pl[i].ScaledValue(column2,cp2) }, pl[i]);
                 }
             }
             Selection paretoFront = new Selection();
             paretoFront.Active = true;
             paretoFront.Label = "Pareto-Front";
-            int zaehler;
 
-            Pavel.GUI.ParetoFinderWindow paretoFinderWindow = new Pavel.GUI.ParetoFinderWindow();
-            paretoFinderWindow.setParetoFinderWindow(sorted.Length -1 , 1);
-            paretoFinderWindow.Show();
+            //TODO: Mixing gui-code within the framework is not really nice, replace this by events
+            PavelMain.MainWindow.StatusBar.StartProgressBar(0, points.Length, "Calculating Paretofront...");
 
-            for (int p1 = 0; p1 < sorted.Length; p1++) {
-                zaehler = 0;
-                for (int p2 = 0; p2 < sorted.Length; p2++) {
-                    if (Dominated3d(sorted[p1], sorted[p2])) // true if i dominated by j // TODO: i? j?
-                        break; 
-                    else
-                        zaehler++;
+            //determine the pareto-front
+            for (int p1 = 0; p1 < points.Length; p1++) {
+                if ( p1 % 100 == 0 ) PavelMain.MainWindow.StatusBar.IncrementProgressBar(100);
+                if ( !points[p1].dominated ) {
+                    for ( int p2 = p1; p2 < points.Length; p2++ ) {
+                        if (!points[p2].dominated){
+                            //Test if the the two points are in an dominating relation
+                            Domination dom = Dominated3d(points[p1], points[p2]);
+                            if ( dom == Domination.Dominated ) {
+                                //p1 is dominated by p2, p1 cannot be pareto-optimal, ignore it
+                                //and exit the loop, all points that are dominated by p1 must by dominated by p2 too.
+                                points[p1].dominated = true;
+                                break;
+                            } else if ( dom == Domination.Dominating ) {
+                                //p2 is domiated by p1, p2 cannot be pareto-optimal
+                                //ignore it in the future
+                                points[p2].dominated = true;
+                            }
+                        }
+                    }
                 }
-                if (zaehler == sorted.Length) { paretoFront.Add(sorted[p1].Point); }
-                paretoFinderWindow.progressBarStep();
-                paretoFinderWindow.Refresh();
             }
-            paretoFinderWindow.Close();
+            //Get all not dominated points from the array
+            for ( int p1 = 0; p1 < points.Length; p1++ ) {
+                if ( !points[p1].dominated ) {
+                    paretoFront.Add(points[p1].point);
+                }
+            }
+            PavelMain.MainWindow.StatusBar.EndProgressBar();
             return paretoFront;
         }
 
+        private enum Domination {Dominated, Dominating, None};
 
-        // true wenn p durch q dominiert wird
-        private static bool Dominated3d(SortablePoint p, SortablePoint q) {
-            if  (    ((q.Values[0] <= p.Values[0]) && (q.Values[1] <  p.Values[1]) && (q.Values[2] <  p.Values[2]))
-                  || ((q.Values[0] <  p.Values[0]) && (q.Values[1] <= p.Values[1]) && (q.Values[2] <  p.Values[2]))
-                  || ((q.Values[0] <  p.Values[0]) && (q.Values[1] <  p.Values[1]) && (q.Values[2] <= p.Values[2]))
-                  && (!(q.Equals(p))) ) { //TODO: Operator precedence correct?
-                return true;
-            } else {
-                return false;
-            }
+        /// <summary>
+        /// Return the domination relation between two points.
+        /// Either q is dominated by q or vice versa, or they are non-comparible by the criteria of pareto-optimality
+        /// </summary>
+        /// <param name="p">first point</param>
+        /// <param name="q">second point</param>
+        /// <returns>Dominated if q dominates p, Dominating if p dominates q, none otherwise</returns>
+        private static Domination Dominated3d(ParetoPoint p, ParetoPoint q) {
+            //All values must be smaller or equal, at least one must be smaller to be dominated
+            if ( q.values[0] <= p.values[0] && q.values[1] <= p.values[1] && q.values[2] <= p.values[2] &&
+                (q.values[0] < p.values[0] || q.values[1] < p.values[1] || q.values[2] < p.values[2]) )
+                return Domination.Dominated;
+            //All values must be greater or equal, at least one must be greater to dominate
+            else if ( q.values[0] >= p.values[0] && q.values[1] >= p.values[1] && q.values[2] >= p.values[2] &&
+                    (q.values[0] > p.values[0] || q.values[1] > p.values[1] || q.values[2] > p.values[2]) )
+                    return Domination.Dominating;
+            return Domination.None;
         }
-
-        private static void SetMaximumFrontNumber(SortablePoint p1, SortablePoint p2) {
-            p1.FrontNumber = Maximum(p1.FrontNumber, (p2.FrontNumber +1));
-        }
-
-        #region Helper Methods
-
-        private static double Median(List<SortablePoint> s, int dimension) {
-            double temp = 0;
-            foreach (SortablePoint p in s) {
-                temp = temp + p.Values[dimension];
-            }
-            temp = (temp / s.Count);
-            return temp;
-        }
-
-        private static int Maximum(int m, int n) {
-            if (m < n) return n;
-            if (m > n) return m; // TODO unneccessary line (unnecessary method)
-            else       return m;
-        }
-
-        private static double Maximum(List<SortablePoint> s, int dimension) {
-            double temp = s[0].Values[dimension];
-
-            foreach (SortablePoint p in s) {
-                if (p.Values[dimension] > temp) { temp = p.Values[dimension]; }
-            }
-            return temp;
-        }
-
-        private static double Minimum(List<SortablePoint> s, int dimension) {
-            double temp = s[0].Values[dimension];
-
-            foreach (SortablePoint p in s) {
-                if (p.Values[dimension] < temp) { temp = p.Values[dimension]; }
-            }
-            return temp;
-        }
-
-        #endregion
 
         #region Comparer
 
-        private static int Comparer2D(SortablePoint p1, SortablePoint p2) {
-            if (p1.Values[0] > p2.Values[0]) return 1;
-            if (p1.Values[0] < p2.Values[0]) return -1;
-            if (p1.Values[1] > p2.Values[1]) return 1;
-            if (p1.Values[1] < p2.Values[1]) return -1;
-            return 0;
-        }
-
-        private static int Comparer3D(SortablePoint p1, SortablePoint p2) {
-            if (p1.Values[0] > p2.Values[0]) return 1;
-            if (p1.Values[0] < p2.Values[0]) return -1;
-            if (p1.Values[1] > p2.Values[1]) return 1;
-            if (p1.Values[1] < p2.Values[1]) return -1;
-            if (p1.Values[2] > p2.Values[2]) return 1;
-            if (p1.Values[2] < p2.Values[2]) return -1;
+        private static int Comparer2D(ParetoPoint p1, ParetoPoint p2) {
+            if (p1.values[0] > p2.values[0]) return 1;
+            if (p1.values[0] < p2.values[0]) return -1;
+            if (p1.values[1] > p2.values[1]) return 1;
+            if (p1.values[1] < p2.values[1]) return -1;
             return 0;
         }
 
         #endregion
 
-        struct SortablePoint {
-            public double[] Values;
-            public Point    Point;
-            public int      FrontNumber;
-            public SortablePoint(double[] values, Point point, int frontnumber) {
-                this.Values      = values;
-                this.Point       = point;
-                this.FrontNumber = frontnumber;
+        /// <summary>
+        /// Container for storing some informations
+        /// </summary>
+        private class ParetoPoint {
+            public double[] values;
+            public Point    point;
+            public bool     dominated;
+            public ParetoPoint(double[] values, Point point) {
+                this.values      = values;
+                this.point       = point;
+                this.dominated = false;
             }
         }
     }
